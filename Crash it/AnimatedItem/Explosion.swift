97e35114277521ebec4ece0abe804738fa7e9626
Olivier Picard <olivier_picard@icloud.com>
2018-03-24 17:17:30 +0400

    Ajout d'une classe 'Explosion' qui hérite de AnimatedItem ; Regroupement des AnimatedItem

---
+++ Crash it/AnimatedItem/Explosion.swift
@@ -0,0 +1,13 @@
+//
+//  Explosion.swift
+//  Crash it
+//
+//  Created by Olivier Picard on 24/03/2018.
+//  Copyright © 2018 Olivier Picard. All rights reserved.
+//
+
+import Foundation
+
+class Explosion: AnimatedItem {
+    
+}
